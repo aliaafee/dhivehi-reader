7209f3eed51f11e7a7ebea8fd1ccd471f89a016f
Ali Aafee <ali.aafee@gmail.com>
2021-06-05 16:54:53 +0545

    Fixed a scraping bug

--- app/web_scrape/scrapers/mihaaru.py
+++ app/web_scrape/scrapers/mihaaru.py
@@ -8,18 +8,32 @@ def scrape(html_text):
 
     soup = BeautifulSoup(html_text, 'html.parser')
 
-    title = soup.find_all(class_='text-40px sm:text-50px text-waheed text-black-two', limit=1)[0]
+    result = {
+        'title': "",
+        'author': "",
+        'time': "",
+        'content': []
+    }
 
-    author = soup.find_all(class_="text-18px sm:leading-none text-waheed text-black-two", limit=1)[0]
+    try:
+        title_elem = soup.find_all(class_='text-40px sm:text-50px text-waheed text-black-two', limit=1)
+        if title_elem:
+            result['title'] = title_elem[0].text
 
-    time = soup.find_all(class_="mx-3 mt-1 leading-none sm:mx-0 text-faseyha", limit=1)[0]
+        author_elem = soup.find_all(class_="text-18px sm:leading-none text-waheed text-black-two", limit=1)
+        if author_elem:
+            result['author'] = author_elem[0].text
 
-    content = soup.find_all(class_="max-w-3xl px-4 mb-8 text-19px leading-loose text-black-two text-faseyha sm:px-0")
+        time_elem = soup.find_all(class_="mx-3 mt-1 leading-none sm:mx-0 text-faseyha", limit=1)
+        if time_elem:
+            result['time'] = time_elem[0].text
+
+        content_elem = soup.find_all(class_="max-w-3xl px-4 mb-8 text-19px leading-loose text-black-two text-faseyha sm:px-0")
+        if content_elem:
+            result['content'] = [item.text for item in content_elem]
+    except:
+        print("Scraping failed")
+        pass
 
     print("Done")
-    return {
-        'title': title.text,
-        'author': author.text,
-        'time': time.text,
-        'content': [item.text for item in content]
-    }
+    return result
