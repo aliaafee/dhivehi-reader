5a6317cbdcdb95de7c55c3501270ba57e09cb213
Ali Aafee <ali.aafee@gmail.com>
2021-06-05 17:20:38 +0545

    Fixed a thing

--- app/web_scrape/scrapers/mihaaru.py
+++ app/web_scrape/scrapers/mihaaru.py
@@ -20,6 +20,10 @@ def scrape(html_text):
         if title_elem:
             result['title'] = title_elem[0].text
 
+        content_elem = soup.find_all(class_="max-w-3xl px-4 mb-8 text-19px leading-loose text-black-two text-faseyha sm:px-0")
+        if content_elem:
+            result['content'] = [item.text for item in content_elem]
+
         author_elem = soup.find_all(class_="text-18px sm:leading-none text-waheed text-black-two", limit=1)
         if author_elem:
             result['author'] = author_elem[0].text
@@ -27,10 +31,6 @@ def scrape(html_text):
         time_elem = soup.find_all(class_="mx-3 mt-1 leading-none sm:mx-0 text-faseyha", limit=1)
         if time_elem:
             result['time'] = time_elem[0].text
-
-        content_elem = soup.find_all(class_="max-w-3xl px-4 mb-8 text-19px leading-loose text-black-two text-faseyha sm:px-0")
-        if content_elem:
-            result['content'] = [item.text for item in content_elem]
     except:
         print("Scraping failed")
         pass
